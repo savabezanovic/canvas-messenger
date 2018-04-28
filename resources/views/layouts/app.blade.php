fff05a7717c1cd7d3f01589fe2ac9f9ad268c9f9
savabezanovic <savabezanovic@hotmail.com>
2018-04-28 21:01:52 +0200

    code problem fixed, node server.js port problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,15 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style type="text/css">
+        #messages{
+            border: 1px solid black;
+            height: 300px;
+            margin-bottom: 8px;
+            overflow: scroll;
+            padding: 5px;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -72,9 +81,41 @@
         </nav>
 
         @yield('content')
+
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://cdn.socket.io/socket.io-1.3.4.js"></script>
+    <script>
+        var socket = io.connect('http://localhost:8890');
+        socket.on('message', function (data) {
+            data = jQuery.parseJSON(data);
+            console.log(data.user);
+            $( "#messages" ).append( "<strong>"+data.user+":</strong><p>"+data.message+"</p>" );
+        });
+        $(".send-msg").click(function(e){
+            e.preventDefault();
+            var token = $("input[name='_token']").val();
+            var user = $("input[name='user']").val();
+            var msg = $(".msg").val();
+            if(msg != ''){
+                $.ajax({
+                    type: "POST",
+                    url: '{!! URL::to("sendmessage") !!}',
+                    dataType: "json",
+                    data: {'_token':token,'message':msg,'user':user},
+                    success:function(data){
+                        console.log(data);
+                        $(".msg").val('');
+                    }
+                });
+            }else{
+                alert("Please Add Message.");
+            }
+        })
+    </script>
 </body>
 </html>
